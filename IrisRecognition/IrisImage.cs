a2a0ce4c32c70a688e24e591df01d2f5759fa71f
akshaychannabasaiah <aksc.champ@gmail.com>
2014-03-18 23:40:01 +0530

    first commit

---
+++ IrisRecognition/IrisImage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Emgu.CV;
+using System.Drawing;
+
+public class IrisImage
+{
+    //private Image<> object
+    //constructor
+	public IrisImage()
+	{
+        //make modification such that it has to take image from file location, or Database or WEbcam
+        //write overloaded constructors.
+        //do only what is possible now. leave other cases undefined
+	}
+    public void GetImageFromWebCam()
+    {
+
+    }
+
+    public void GetImageFromFile(string fileLocation)
+    {
+
+    }
+    
+    public void GetImageFromDB(string userName, string password)
+    {
+
+    }
+
+    public Image< > GetImage()
+    {
+        //   return Image<>;
+    }
+    public void PerformXXoverImage()
+    {
+        //XX is replaced by various operations,
+        //like edge detection etc.
+    }
+
+}
